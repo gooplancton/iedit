af6a54420f27d1deba065fd79530c6f889775e59
gooplancton <gooplancton@outlook.com>
2025-10-24 20:26:21 +0800

    fix: corrected github actions

--- Formula/iedit.rb
+++ Formula/iedit.rb
@@ -7,10 +7,10 @@ class Iedit < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/gooplancton/iedit/releases/download/v#{version}/iedit-macos-x86_64.tar.gz"
+      url "https://github.com/gooplancton/iedit/releases/download/v#{version}/iedit-macos-x86_64"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_x86_64_TAR_GZ"
     else
-      url "https://github.com/gooplancton/iedit/releases/download/v#{version}/iedit-macos-arm64.tar.gz"
+      url "https://github.com/gooplancton/iedit/releases/download/v#{version}/iedit-macos-arm64"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_arm64_TAR_GZ"
     end
 
@@ -22,7 +22,7 @@ class Iedit < Formula
 
   on_linux do
     # For Linux/Homebrew on Linux we build from source by default.
-    url "https://github.com/gooplancton/iedit/archive/refs/tags/v#{version}.tar.gz"
+    url "https://github.com/gooplancton/iedit/archive/refs/tags/v#{version}"
     sha256 "REPLACE_WITH_SOURCE_TARBALL_SHA256"
 
     depends_on "rust" => :build
